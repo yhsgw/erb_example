ecb49eabf73357b65396c5c3e94aecb1575a52a9
yhsgw <yhsgw.n@gmail.com>
2013-07-09 21:30:14 +0900

    add def_erb_method example

--- example.rb
+++ example.rb
@@ -1,9 +1,24 @@
 #-*- coding: utf-8 -*-
 #
-
+#
 require 'erb'
+require 'date'
 
+#クラスにメソッド定義をして使用する例
+class Foo
+  extend ERB::DefMethod
+
+  def initialize()
+    @today = DateTime.now
+  end
+
+  def_erb_method('hello(name)','./template/hello.erb')
+end
+
+puts "Foo#hello => #{Foo.new.hello('yhsgw')}"
+
+#テンプレートを読み込んで、値を埋め込む単純な例
 something = "埋め込み表示用テキスト"
 erb = File.open('./template/template.erb'){|f| ERB.new(f.read)}
-puts erb.result
+puts "template.erb => **********\n#{erb.result}\n**********"
 
